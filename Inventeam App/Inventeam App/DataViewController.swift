d9d43a62f81d325611adecc438de67339d89ea15
samanthac02 <72635022+samanthac02@users.noreply.github.com>
2022-06-11 11:07:56 -0700

    Added scuffed Data page

--- Inventeam App/Inventeam App/DataViewController.swift
+++ Inventeam App/Inventeam App/DataViewController.swift
@@ -9,21 +9,41 @@ import UIKit
 
 class DataViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var BPMLabel: UILabel!
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { _ in
+            let url = URL(string: "http://192.168.4.1/read")
+            let urlreq=URLRequest(url: url!)
+            self.webView.loadRequest(urlreq)
+            
+            if let url = URL(string: "http://192.168.4.1/read") {
+                do {
+                    var contents = try String(contentsOf: url)
+                    contents = contents.replacingOccurrences(of: "<!DOCTYPE HTML>", with: "")
+                    contents = contents.replacingOccurrences(of: "<html>", with: "")
+                    contents = contents.replacingOccurrences(of: "\n", with: "")
+                    contents = contents.replacingOccurrences(of: " ", with: "")
+                    contents = contents.replacingOccurrences(of: "\r", with: "")
+                    self.BPMLabel.text = contents
+                    
+                    print(contents)
+                    
+                    if Int(contents) ?? 0 > 100 {
+                        print("JELLO")
+                        self.BPMLabel.text = "UR KID IS DYING"
+                    }
+                } catch {
+                    // contents could not be loaded
+                }
+            } else {
+                // the URL was bad!
+            }
+        })
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
